3d9b884b70c925a7b25afbba7a24171eddda670c
Necra2016 <jeremy18cote@gmail.com>
2025-04-23 15:47:29 -0400

    a

--- Assets/Scripts/MiniJeu.cs
+++ Assets/Scripts/MiniJeu.cs
@@ -2,14 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MiniJeu : MonoBehaviour
 {
     public float pointageTemps;
     public TextMeshProUGUI textScore;
+    public TMP_InputField inputNom;
+    public TextMeshProUGUI textScorePanneau;
+    [SerializeField] private GameObject panneauRecord;
 
     void Start()
     {
+        PlayerPrefs.DeleteAll();
         pointageTemps = 0;
     }
 
@@ -17,4 +22,30 @@ public class MiniJeu : MonoBehaviour
     {
         textScore.text = pointageTemps.ToString("00.00");
     }
+
+    public void  TraiterMort()
+    {
+        float recordActuel = PlayerPrefs.GetFloat("meilleurScore", 0f);
+        print("defaite");
+        if (pointageTemps >= recordActuel)
+        {
+            Invoke("MontrePanneauNouveauRecord", 3f);
+        }
+    }
+
+    void MontrePanneauNouveauRecord()
+    {
+        print("NvR");
+        panneauRecord.SetActive(true);
+        textScorePanneau.text = textScore.text;
+    }
+
+    public void EnregistrerNomRecord()
+    {
+        string nom = inputNom.text;
+        PlayerPrefs.SetString("Nom", nom);
+
+        //Redemarre la scene actuler
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    } 
 }
